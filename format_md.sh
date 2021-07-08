e8780974dcf478c0ef5de7ddc76da9320b75daa3
Michael Kirk <michael.code@endoftheworl.de>
2021-07-08 11:58:32 -0700

    fix formatting scripts for my machine
    I seem to have be deps in different places using npm install/run ensures
    we have the same deps
    
    Also updated the fmt script to take an arg or not, and runs a bit faster
    for the 'format all' case by passing all args into a single invocation

--- format_md.sh
+++ format_md.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 set -e
-~/npm/node_modules/prettier/bin-prettier.js --write --prose-wrap=always $1
 
-# Format everything:
-# for x in `find book/src/ | grep '\.md'`; do ./format_md.sh $x; done; git checkout book/src/project/CHANGELOG.md
+if [ -z "$@" ]
+then
+
+echo "formatting everything"
+find book/src -name \*.md \! -name CHANGELOG.md | xargs npm run fmt
+
+else
+
+echo "formatting $@"
+npm run fmt "$@"
+fi
